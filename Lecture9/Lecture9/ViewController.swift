84f7bf5aa598055f4c1b38ff1dffe8a14b3c74c2
Monica Ong <myo3@cornell.edu>
2017-04-24 19:58:39 -0400

    Intialized project

---
+++ Lecture9/Lecture9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture9
+//
+//  Created by Monica Ong on 4/24/17.
+//  Copyright © 2017 Monica Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
